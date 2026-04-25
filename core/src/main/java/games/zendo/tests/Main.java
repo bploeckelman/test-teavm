9383d67484ed2cbde7886c7c3ac52faeb7dada07
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2026-04-25 09:29:07 -0500

    teavm: infinite loop minimal repro

--- core/src/main/java/games/zendo/tests/Main.java
+++ core/src/main/java/games/zendo/tests/Main.java
@@ -2,6 +2,8 @@ package games.zendo.tests;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.Texture;
 import com.kotcrab.vis.ui.VisUI;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
@@ -16,13 +18,20 @@ public class Main extends Game {
         //  - doesn't matter if built-in (library classpath) skin or same skin loaded from internal assets
         //  - doesn't matter if TeaVMBuilder includes .addReflectionClass(...) with same
         //  - with correct reflection classes, still fails to load built-in classpath skin
-
-//        VisUI.setSkipGdxVersionCheck(true);
-
-        // Load built-in skin from library classpath
-//        VisUI.load();
-        // Load built-in skin from internal assets (same files as built-in skin from library)
+        // ***FAILS***: Load built-in skin from library classpath
+        //VisUI.load();
+        // ***WORKS*** (with correct reflection classes): Load built-in skin from internal assets (same files as built-in skin from library)
         VisUI.load(Gdx.files.internal("skins/visui-1x-default/uiskin.json"));
+
+        // --------------------------------------------------------------------
+        // TeaVM Infinite Loop
+        // - LWJGL3: GdxRuntimeException: Couldn't load dependencies of asset: does-not-exist.png
+        // - GWT: GdxRuntimeException: Could not submit AsyncTask: Couldn't load image 'does-not-exist.png', file does not exist
+        // - TeaVM: black screen, infinite loop, browser hangs
+        var mgr = new AssetManager();
+        mgr.load("does-not-exist.png", Texture.class);
+        mgr.finishLoading();
+        // --------------------------------------------------------------------
 
         setScreen(new FirstScreen());
     }
